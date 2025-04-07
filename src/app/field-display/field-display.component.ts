bf9fd3a3b1917946587202033d6e33561ddf46da
Dennis Gonzalez <dennisgzlz88@gmail.com>
2025-04-07 16:18:33 -0500

    added components and json editor

--- src/app/field-display/field-display.component.ts
+++ src/app/field-display/field-display.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { BaseField, SectionField } from '../models/form.model';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './field-types/input/input.component';
 
 @Component({
   selector: 'app-field-display',
-  imports: [CommonModule],
+  imports: [CommonModule, InputComponent],
   templateUrl: './field-display.component.html',
   styleUrl: './field-display.component.scss'
 })
---
+++ src/app/field-display/field-types/input/input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  template: `
+    <p style="margin: 0px">
+      input <button mat-button>Click me!</button>
+    </p>
+  `,
+  styles: ``
+})
+export class InputComponent {
+
+}
